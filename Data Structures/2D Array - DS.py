14a71d6f03fe430574444d40738a88bd56229010
Vipul Munot <vipmunot@gmail.com>
2017-02-03 13:25:56 -0500

    adding data structures

---
+++ Data Structures/2D Array - DS.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+
+arr = []
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+total = -9999
+for i in range(1,5):
+    for j in range(1,5):
+        sum = arr[i][j]+arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]+arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1]
+        if sum > total: total = sum
+print(total)           
